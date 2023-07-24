5091468a99911b36fa533819eaa0c666a5ebb4e7
Sarah Almutlaq <140312973+SarahAlmutlaq00@users.noreply.github.com>
2023-07-24 23:11:35 +0300

    Add files via upload

---
+++ Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+//Task 1: Variable Practice
+var score: Int
+score = 10
+print(score)
+
+//Task 2: Control Flow
+if score > 9 {
+    print("pass")
+}else{
+    print("fail")
+}
+
+//Task 3: Loop Practice
+let Names : [String] = ["sarah","shatha","norah","rahaf","shahad"]
+for names in Names {
+    print(names)
+}
+
+//Task 4: Working with Dictionarie
+var Fruit : [String:Int] = ["Apple":3,"Kiwi":5,"Cherry":2,"Banana":1]
+for fruit in Fruit{
+    print(fruit)
+}
+
+//Task 5: String Interpolation
+let name : String = "sarah"
+print("welcome  \(name)")
+
